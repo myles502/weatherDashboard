cc80b5049b0e34473b051ddc530f46a2b5798bed
myles502 <80232038+myles502@users.noreply.github.com>
2021-03-29 23:55:41 -0700

    finalchanges

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 $(function () {
- 
+  function renderForecast(data) {
+    
+    $("#city-name").text(data.city.name);
+    var tbody = $("#forecast-table-body").empty();
+    var forecastEntry
+    for (var i = 0; i < data.list.length; i += 1) {
+       forecastEntry = data.list[i];
+       var ts = forecastEntry.dt * 1000;
+       var date = new Date(ts);
+       var weatherIconImg = $("<td>").attr({
+         src : "http://openweathermap.org/img/wn" + forecastEntry.weather[0].icon + ".png"
+       })
+       var weatherIconCell = $("<td>").append(dateCell,weatherIconCell);
+       var tempCell = $("<td>").html(Math.round(forecastEntry.main.temp + "&deg; F"));
+    
+       var tr = $("<tr>").append(dateCell, weatherIconCell, tempCell);
+       var month = date.getMonth() + 1;
+       var day = date.getDate();
+       var hour = date.getHours();
+       var minutes = date.getMinutes();
+       if (minutes < 2) {
+         minutes = "0" + minutes;
+       }
+       var time = date.toLocaleTimeString();
+       var dateCell = $("<td>").append(month + "/" + day, "<br>", hour + ":" + minutes);
+       
+     var tr = $("<tr>").append(dateCell);
+     tbody.append(tr);
+    }
+    $("#forecast-results").removeClass(".d-none");
+  }
+  
+  
+  
+  
+  
   function fetchCityForecast(city) {
     var openweatherApiKey = "5411c2401f72d7130e304464c4b927a2";
     var url = "https://api.openweathermap.org/data/2.5/forecast?appid=" + openweatherApiKey;
@@ -9,10 +44,11 @@ $(function () {
         $.ajax({
            url: url,
            method: "GET"
-        }).done(function(data) {
-          
-          console.log(data);
-        }).fail(function(request) {
+        })
+          .done(function(data) {
+           renderForecast(data);
+        })
+        .fail(function(request) {
       
           if (request.status === 404) {
             alert("could not find location");
