28d9cf01088b8c3d346e36b8f861f0e8687a06a8
myles502 <80232038+myles502@users.noreply.github.com>
2021-03-29 21:48:08 -0700

    updated error message

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-
-$(function(){
+$(function () {
  
   function fetchCityForecast(city) {
     var openweatherApiKey = "5411c2401f72d7130e304464c4b927a2";
-    var url = "api.openweathermap.org/data/2.5/forecast?appid=" + openweatherApiKey;
+    var url = "https://api.openweathermap.org/data/2.5/forecast?appid=" + openweatherApiKey;
     url += "&q=" + city ;
     url += "&unit=imperial"
     
-    $.ajax({
-      url: url,
-      method: "GET"
-    }).done(function(data) {
-       console.log(data);
-    })
-
-    
-  } 
-
+        $.ajax({
+           url: url,
+           method: "GET"
+        }).done(function(data) {
+          
+          console.log(data);
+        }).fail(function(request) {
+      
+          if (request.status === 404) {
+            alert("could not find location");
+          } else { 
+            alert ("could not find location")
+          }   
+        })
+       
+ 
+  }
 
   $(".form-inline").on("submit",function(event){
    event.preventDefault();
@@ -30,9 +36,6 @@ $(function(){
    }
   
   });
-
-
-
 
 
 });
