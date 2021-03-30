bd0fe07b89ee92f502ae97514d66b0a7195cb55b
myles502 <80232038+myles502@users.noreply.github.com>
2021-03-29 19:41:32 -0700

    updatesto javascript, ajax

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 
 $(function(){
  
+  function fetchCityForecast(city) {
+    var openweatherApiKey = "5411c2401f72d7130e304464c4b927a2";
+    var url = "api.openweathermap.org/data/2.5/forecast?appid=" + openweatherApiKey;
+    url += "&q=" + city ;
+    url += "&unit=imperial"
+    
+    $.ajax({
+      url: url,
+      method: "GET"
+    }).done(function(data) {
+       console.log(data);
+    })
+
+    
+  } 
+
+
   $(".form-inline").on("submit",function(event){
-   event.preventDefault ();
+   event.preventDefault();
    
    var cityInput = $("#city-input");
    var city = cityInput.val().trim();
 
    if (city !== "") {
-     alert(city);
+     fetchCityForecast(city);
      cityInput.val("");
    }
   
@@ -16,7 +33,6 @@ $(function(){
 
 
 
+
+
 });
-
-
-
